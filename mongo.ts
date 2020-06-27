39448525ad1e32da0646333af92f518d7e112d68
Shivam Mehta <shivam.mehta2019@vitstudent.ac.in>
2020-06-27 13:00:38 +0530

    final

--- mongo.ts
+++ mongo.ts
@@ -2,7 +2,7 @@ import { MongoClient } from "https://deno.land/x/mongo@v0.8.0/mod.ts";
 
 const client = new MongoClient();
 client.connectWithUri(
-  "mongodb+srv://deno:denopassword@cluster0-tnmyh.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority",
+  "INSERT MONGO URI HERE",
 );
 
 const db = client.database("test");
